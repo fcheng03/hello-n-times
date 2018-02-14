82501ca0dc13809127932bd1d3966df1c19b5722
Francis C <fcheng03@nyit.edu>
2018-02-14 11:38:15 -0500

    your commit message

--- src/Hello.java
+++ src/Hello.java
@@ -3,10 +3,15 @@ public class Hello
     public static void hello(String message, int count)
     {
         // your code here
+        for (int counter = 0; counter < count; counter++) {
+            System.out.println(message);
+        }
     }
 
     public static void main(String[] args)
     {
         hello("hello world", 4);
     }
+
+
 }
